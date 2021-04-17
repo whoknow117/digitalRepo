8ce5408e34213ae20e74173a9a7b58119b0422f8
newbeeguy <mskjavascript@gmail.com>
2021-04-17 20:59:40 +0300

    dialect

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ module.exports = new Sequelize(
     process.env.DB_PASSWORD,
     {
 
-       
+        dialect: 'postgres',
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
         dialectOptions:{
