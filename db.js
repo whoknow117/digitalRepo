dae71d18702c22a6c5b81bf0abd0c2fe8dbe515c
newbeeguy <mskjavascript@gmail.com>
2021-04-18 07:24:38 +0300

    sasd

--- db.js
+++ db.js
@@ -1,20 +1,13 @@
 const {Sequelize} = require('sequelize')
 
+
 module.exports = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-
         dialect: 'postgres',
         host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialectOptions:{
-
-            ssl: false
-            
-
-        }
+        port: process.env.DB_PORT
     }
-
 )
